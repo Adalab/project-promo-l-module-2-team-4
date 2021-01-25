2b6098cf26e8bff0f264a691a2aa5258ddb3a4a8
AmandaKhol <amanda.camsua@gmail.com>
2021-01-25 20:04:45 +0100

    add js to name and email link

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const nameElement = document.querySelector(".js-name");
+const namePreviewElement = document.querySelector(".js-preview-name");
+
+const emailElement = document.querySelector(".js-email");
+let emailPreviewElement = document.querySelector(".js-preview-email");
+
+function handleName(event) {
+  const nameValue = event.target.value;
+  if (nameValue === "") {
+    namePreviewElement.innerHTML = "Nombre Apellido";
+  } else {
+    namePreviewElement.innerHTML = nameValue;
+    console.log(namePreviewElement.href);
+  }
+}
+function handleEmail(event) {
+  const emailValue = event.target.value;
+  if (emailValue === "") {
+    emailPreviewElement.href = "";
+  } else {
+    emailPreviewElement.href = `mailto:${emailValue}`;
+    emailPreviewElement.title = `mailto:${emailValue}`;
+    console.log(emailPreviewElement.href);
+  }
+}
+
+nameElement.addEventListener("keyup", handleName);
+emailElement.addEventListener("keyup", handleEmail);
