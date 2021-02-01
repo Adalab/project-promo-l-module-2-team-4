ea8849f3ea2431f9673487fd729c0cd2171919a2
Dandara Almeida <dandara.avazquez@gmail.com>
2021-02-01 21:27:14 +0100

    Add palette color interaction with preview card

--- src/js/main.js
+++ src/js/main.js
@@ -31,3 +31,21 @@ function handleEmail(event) {
 
 nameElement.addEventListener("keyup", handleName);
 emailElement.addEventListener("keyup", handleEmail);
+
+
+// PALETTE INTERACTION:
+
+const paletteInputElement = document.querySelectorAll('.js_palette-select');
+const cardPreviewElement = document.querySelector('.js_preview-card');
+
+function handlePalette(){
+  cardPreviewElement.classList.remove('palette-1', 'palette-2', 'palette-3');
+  let checkedPalette = document.querySelector('.js_palette-select:checked');
+  let checkedPaletteValue = checkedPalette.value;
+  cardPreviewElement.classList.add('palette-' + checkedPaletteValue);
+}
+
+for (const paletteElement of paletteInputElement) {
+  paletteElement.addEventListener('change', handlePalette);
+}
+handlePalette();
