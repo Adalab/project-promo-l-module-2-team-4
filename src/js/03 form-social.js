0681d260a8818541b0bbe2d9ee36f6bb17f3fde7
Nadia Murias <nadiamurias@gmail.com>
2021-02-02 12:29:46 +0100

    Add js form social links

--- src/js/03 form-social.js
+++ src/js/03 form-social.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const linkedinElement = document.querySelector(".js-linkedin");
+const linkedinPreviewElement = document.querySelector('.js-preview-linkedin');
+
+const githubElement = document.querySelector('.js-github');
+const githubPreviewElement = document.querySelector('.js-preview-github');
+
+function handleLinkedin(){
+    const linkedinValue = linkedinElement.value;
+   if (linkedinValue === ""){
+   linkedinPreviewElement.href = "";
+   console.log(linkedinValue);
+} 
+    else {
+    linkedinPreviewElement.href = linkedinValue;
+    linkedinPreviewElement.title = linkedinValue;
+}
+
+function handleGithub(){
+    const githubValue = githubElement.value;
+    if (githubValue === "") {
+        githubPreviewElement.href="";
+        console.log(githubValue);
+    }
+    else {
+        githubPreviewElement.href= githubValue;
+        githubPreviewElement.title= githubValue;
+    }
+
+}
+
+linkedinElement.addEventListener('keyup',handleLinkedin);
+githubElement.addEventListener('keyup',handleGithub);
--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,33 @@
-"use strict";
+// "use strict";
 
-const nameElement = document.querySelector(".js-name");
-const namePreviewElement = document.querySelector(".js-preview-name");
+// const nameElement = document.querySelector(".js-name");
+// const namePreviewElement = document.querySelector(".js-preview-name");
 
-const emailElement = document.querySelector(".js-email");
-let emailPreviewElement = document.querySelector(".js-preview-email");
+// const emailElement = document.querySelector(".js-email");
+// let emailPreviewElement = document.querySelector(".js-preview-email");
 
-function handleName(event) {
-  console.log("entra en 1");
-  const nameValue = event.target.value;
-  console.log(nameValue);
-  if (nameValue === "") {
-    namePreviewElement.innerHTML = "Nombre Apellido";
-  } else {
-    namePreviewElement.innerHTML = nameValue;
-    console.log("ha entrado en else");
-  }
-}
-function handleEmail(event) {
-  console.log("entra en 2");
-  const emailValue = event.target.value;
-  if (emailValue === "") {
-    emailPreviewElement.href = "";
-  } else {
-    emailPreviewElement.href = `mailto:${emailValue}`;
-    emailPreviewElement.title = `mailto:${emailValue}`;
-    console.log(emailPreviewElement.href);
-  }
-}
+// function handleName(event) {
+//   console.log("entra en 1");
+//   const nameValue = event.target.value;
+//   console.log(nameValue);
+//   if (nameValue === "") {
+//     namePreviewElement.innerHTML = "Nombre Apellido";
+//   } else {
+//     namePreviewElement.innerHTML = nameValue;
+//     console.log("ha entrado en else");
+//   }
+// }
+// function handleEmail(event) {
+//   console.log("entra en 2");
+//   const emailValue = event.target.value;
+//   if (emailValue === "") {
+//     emailPreviewElement.href = "";
+//   } else {
+//     emailPreviewElement.href = `mailto:${emailValue}`;
+//     emailPreviewElement.title = `mailto:${emailValue}`;
+//     console.log(emailPreviewElement.href);
+//   }
+// }
 
-nameElement.addEventListener("keyup", handleName);
-emailElement.addEventListener("keyup", handleEmail);
+// nameElement.addEventListener("keyup", handleName);
+// emailElement.addEventListener("keyup", handleEmail);
