551449ee737d7e2bc918ed8834d6be4bff25c1fd
Nadia Murias <nadiamurias@gmail.com>
2021-02-02 12:50:59 +0100

    fix conflict js

--- src/js/03 form-social.js
+++ src/js/03 form-social.js
@@ -1,32 +1,33 @@
-"use strict";
+'use strict';
 
-const linkedinElement = document.querySelector(".js-linkedin");
+const linkedinElement = document.querySelector('.js-linkedin');
 const linkedinPreviewElement = document.querySelector('.js-preview-linkedin');
 
 const githubElement = document.querySelector('.js-github');
 const githubPreviewElement = document.querySelector('.js-preview-github');
 
 function handleLinkedin(){
-    const linkedinValue = linkedinElement.value;
-   if (linkedinValue === ""){
-   linkedinPreviewElement.href = "";
-   console.log(linkedinValue);
-} 
-    else {
+  const linkedinValue = linkedinElement.value;
+  if (linkedinValue === ''){
+    linkedinPreviewElement.href = '';
+    console.log(linkedinValue);
+  }
+  else {
     linkedinPreviewElement.href = linkedinValue;
     linkedinPreviewElement.title = linkedinValue;
+  }
 }
 
 function handleGithub(){
-    const githubValue = githubElement.value;
-    if (githubValue === "") {
-        githubPreviewElement.href="";
-        console.log(githubValue);
-    }
-    else {
-        githubPreviewElement.href= githubValue;
-        githubPreviewElement.title= githubValue;
-    }
+  const githubValue = githubElement.value;
+  if (githubValue === '') {
+    githubPreviewElement.href='';
+    console.log(githubValue);
+  }
+  else {
+    githubPreviewElement.href= githubValue;
+    githubPreviewElement.title= githubValue;
+  }
 
 }
 
--- src/js/04 preview-image.js
+++ src/js/04 preview-image.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
 const fr = new FileReader();
-const uploadBtn = document.querySelector(".js__profile-trigger");
-const fileField = document.querySelector(".js__profile-upload-btn");
-const profileImage = document.querySelector(".js__profile-image");
-const profilePreview = document.querySelector(".js__profile-preview");
+const uploadBtn = document.querySelector('.js__profile-trigger');
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
 
 function getImage(e) {
   const myFile = e.currentTarget.files[0];
-  fr.addEventListener("load", writeImage);
+  fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
 }
 
@@ -22,5 +22,5 @@ function fakeFileClick() {
   fileField.click();
 }
 
-uploadBtn.addEventListener("click", fakeFileClick);
-fileField.addEventListener("change", getImage);
+uploadBtn.addEventListener('click', fakeFileClick);
+fileField.addEventListener('change', getImage);
