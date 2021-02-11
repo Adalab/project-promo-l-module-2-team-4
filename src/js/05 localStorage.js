30b7e101f445cb7807f6257fb3f8c1d22012a9fe
Nadia Murias <nadiamurias@gmail.com>
2021-02-11 11:55:22 +0100

    Add comments in LocalStorage

--- src/js/05 localStorage.js
+++ src/js/05 localStorage.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 
@@ -25,51 +25,51 @@ Recuperar del local storage:
 function saveInLocalStorage() {
   // obtengo los valores de todos los campos
   const userData = {
-    photo: photo,
-    palette: document.querySelector('.js_palette-select:checked').value,
-    name: document.querySelector('.js-name').value,
-    job: document.querySelector('.js-job').value,
-    email: document.querySelector('.js-email').value,
-    phone: document.querySelector('.js-tel').value,
-    linkedin: document.querySelector('.js-linkedin').value,
-    github: document.querySelector('.js-github').value
+    photo: fr.result,
+    palette: document.querySelector(".js_palette-select:checked").value,
+    name: document.querySelector(".js-name").value,
+    job: document.querySelector(".js-job").value,
+    email: document.querySelector(".js-email").value,
+    phone: document.querySelector(".js-tel").value,
+    linkedin: document.querySelector(".js-linkedin").value,
+    github: document.querySelector(".js-github").value,
   };
   // lo convierto a string porque local storage solo admite strings
   const userDataInString = JSON.stringify(userData);
   // lo guardo en el local storage en el campo que me apetece
-  localStorage.setItem('userData', userDataInString);
+  localStorage.setItem("userData", userDataInString);
 }
 
 // al arrancar la página recogemos los datos desde el local storage y actualizamos el formulario
 function getFromLocalStorage() {
   // obtengo los datos desde el local storage
-  const userDataInString = localStorage.getItem('userData');
+  const userDataInString = localStorage.getItem("userData");
   // compruebo si hay datos válidos, es decir si la usuaria ya había entrado en nuestra web anteriormente
   if (userDataInString !== null) {
     const userData = JSON.parse(userDataInString);
     // actualizo los inputs del formulario
-    document.querySelector('.js-name').value = userData.name;
-    document.querySelector('.js-job').value = userData.job;
-    document.querySelector('.js-email').value = userData.email;
-    document.querySelector('.js-tel').value = userData.phone;
-    document.querySelector('.js-linkedin').value = userData.linkedin;
-    document.querySelector('.js-github').value = userData.github;
+    document.querySelector(".js-name").value = userData.name;
+    document.querySelector(".js-job").value = userData.job;
+    document.querySelector(".js-email").value = userData.email;
+    document.querySelector(".js-tel").value = userData.phone;
+    document.querySelector(".js-linkedin").value = userData.linkedin;
+    document.querySelector(".js-github").value = userData.github;
     // actualizo la variable global de la foto
-    photo = userData.photo;
+    // fr.result = userData.photo; REVISARRRRRR
     // actualizo la paleta, para ello
     // - Recorro las 3 paletas
     // - Compruebo cuál tiene el valor que tengo en el local storage
     // - A la paleta que tiene el valor correcto le hago un .checked = true para activarla
-    const paletteElements = document.querySelectorAll('.js_palette-select');
+    const paletteElements = document.querySelectorAll(".js_palette-select");
     for (const paletteElement of paletteElements) {
       if (paletteElement.value === userData.palette) {
         paletteElement.checked = true;
       }
     }
     // propago los datos desde el formulario a la tarjeta
-    updateAllInputs(); 
+    updateAllInputs();
     updatePalette();
-    updatePhoto();
+    // updatePhoto();
   }
 }
 
